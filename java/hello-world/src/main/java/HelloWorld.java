645f6480d91774d5cfa9240fbe8603c2da6f4a3b
johnngugi <johnngugi777@gmail.com>
2016-09-04 21:41:55 +0300

    Add java solution

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,2 +1,14 @@
 public class HelloWorld {
+
+    public static String hello(String name) {
+
+        if (name == null || name == "") {
+            return "Hello, World!";
+        } else {
+            name = "Hello, " + name + "!";
+            return name;
+        }
+
+    }
+
 }
